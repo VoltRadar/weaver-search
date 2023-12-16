ad7c592e2c50730ae87deac61c61c03b41c8397b
VoltRadar <farnsworthdavid2@gmail.com>
2023-12-16 14:03:14 +0000

    WordList class to check if word is valid

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # weaver-search
+
 What's the longest word possible word ladder?
+
+Currently in development
---
+++ src/main.java
@@ -0,0 +1,6 @@
+
+public class main {
+	public main(String[] args) {
+		System.out.print("hello, world");
+	}
+}
