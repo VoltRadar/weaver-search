1f6ab9af8efd6557da0da69f5635c43b1ac6fac5
VoltRadar <farnsworthdavid2@gmail.com>
2023-12-16 20:52:19 +0000

    Finished Java searcher

---
+++ Java/src/Search.java
@@ -0,0 +1,123 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Search {
+	
+	static final WordList list = new WordList();
+	private ArrayList<ArrayList<String>> paths;
+	
+	public Search() {
+		
+	}
+	
+	public Hashtable<String, ArrayList<String>> full_search(String startingWord) {
+		// Sets the path variable to the paths from a given word to every 
+		// other possible words
+		
+		this.paths = new ArrayList<>();
+		this.paths.add(new ArrayList<String>());
+		this.paths.get(0).add(startingWord);
+		
+		@SuppressWarnings("unchecked")
+		ArrayList<String> remainingWords = (ArrayList<String>) list.wordArray.clone();
+		
+		Hashtable<String, ArrayList<String>> seenWords = new Hashtable<>();
+		seenWords.put(startingWord, this.paths.get(0));
+		
+		while (!this.paths.isEmpty()) {
+			// Extend the smallest path
+			ArrayList<String> path = this.paths.remove(0);
+			String lastWord = path.get(path.size() - 1);
+			
+			for (String connectedWord : Search.list.wordTable.get(lastWord)) {
+				if (seenWords.containsKey(connectedWord)) {
+					continue;
+				}
+				
+				@SuppressWarnings("unchecked")
+				ArrayList<String> newPath = (ArrayList<String>) path.clone();
+				newPath.add(connectedWord);
+				
+				// Add the new path to the paths list
+				this.paths.add(newPath);
+				
+				seenWords.put(connectedWord, newPath);
+				remainingWords.remove(connectedWord);
+			}
+		}
+		
+		return seenWords;
+	}
+	
+	public ArrayList<ArrayList<String>> findLongesPaths() {
+		// Finds the longest paths
+		
+		int longestLength = 0;
+		ArrayList<ArrayList<String>> longestPaths = new ArrayList<>();
+		
+		Iterator<String> graphWords = this.full_search("test").keys().asIterator();
+		
+		int count = 0;
+		
+		// For each word you need to check
+		while (graphWords.hasNext()) {
+			String word = graphWords.next();
+			Iterator<ArrayList<String>> pathsFromWord = 
+					this.full_search(word).elements().asIterator();
+			
+			while (pathsFromWord.hasNext()) {
+				ArrayList<String> pathToCheck = pathsFromWord.next();
+				
+				if (pathToCheck.size() >= longestLength) {
+					if (pathToCheck.size() > longestLength) {
+						// Reset the longest length path if you've found a longer one
+						longestLength = pathToCheck.size();
+						longestPaths = new ArrayList<>();
+					}
+				
+					longestPaths.add(pathToCheck);
+				}
+			}
+			
+			count++;
+			if (count % 10 == 0)
+			System.out.println(Integer.toString(count) + ":\t" + word);
+		}
+		
+		return longestPaths;
+	}
+	
+	public static void main(String[] args) {
+		Search s = new Search();
+		
+		if (args.length == 0)
+			// Find the longest optimal path between two words
+			for (ArrayList<String> path : s.findLongesPaths()) {
+				System.out.println(path);
+			}
+		
+		else if (args.length == 2) {
+			for (String word : args) {
+				// Check that
+				if (!Search.list.is_word(word)) {
+					System.err.println(word + " is not in my word list!");
+					System.exit(1);
+				}
+			}
+			
+			Hashtable<String, ArrayList<String>> paths = s.full_search(args[0]);
+			if (!paths.containsKey(args[1])) {
+				System.err.println("Can't find " + args[1] + " from " + args[0] + "!");
+				System.exit(1);
+			}
+			
+			ArrayList<String> path = paths.get(args[1]);
+			System.out.println("Length :" + Integer.toString(path.size() - 1));
+			System.out.println("  Path :" + path.toString());
+		}
+	}
+
+}
---
+++ Java/src/WordList.java
@@ -0,0 +1,141 @@
+// Word list source: https://www.diginoodles.com/projects/eowl
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class WordList {
+	private static String letters = "qwertyuiopasdfghjklzxcvbnm";
+	private static String wordListName = "weaver_words.txt";
+	
+	ArrayList<String> wordArray;
+	
+	// Hash table mapping words to connected words
+	Hashtable<String, ArrayList<String>> wordTable;
+	
+	public WordList() {
+		this.wordArray = new ArrayList<>();
+		this.wordTable = new Hashtable<>();
+		
+		this.read_file();
+		
+		this.create_table();
+		this.make_connections();
+	}
+	
+	private void read_file() {
+		// Open and load the words into the word array
+		
+		
+		// Find the word list
+		String currentDir = System.getProperty("user.dir");
+		Path wordListPath = FileSystems.getDefault().getPath(currentDir);
+		
+		while (wordListPath != null) {
+			boolean found = false;
+			
+			for (File file : wordListPath.toFile().listFiles()) {
+				if (file.toString().contains(WordList.wordListName)) {
+					wordListPath = file.toPath();
+					found = true;
+					break;
+				}
+			}
+			
+			if (found) {
+				break;
+			} else {
+				wordListPath = wordListPath.getParent();
+			}
+		}
+		
+		if (wordListPath == null) {
+			System.err.println("Couldn't find words file!");
+			System.exit(1);
+		}
+		
+		File wordsFile = new File(wordListPath.toString());
+		
+		try {
+			Scanner wordsReader = new Scanner(wordsFile);
+			
+			while (wordsReader.hasNextLine()) {
+				this.wordArray.add(wordsReader.nextLine());
+			}
+			
+			wordsReader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.err.print("Words list not found!");
+			System.exit(1);
+		}
+	}
+	
+	private ArrayList<String> connected_words(String word) {
+		// Calculates the words differing by one letter from the input word
+		
+		ArrayList<String> connectedWords = new ArrayList<>();
+		
+		for (int letterIndex=0; letterIndex < word.length(); letterIndex++) {
+			StringBuffer wordBuffer = new StringBuffer(word);
+			
+			for (int i=0; i < WordList.letters.length(); i++) {
+				wordBuffer.deleteCharAt(letterIndex);
+				wordBuffer.insert(letterIndex, WordList.letters.charAt(i));
+				
+				String newWord = wordBuffer.toString();
+				if (this.is_word(newWord) && !word.equals(newWord))
+					connectedWords.add(newWord);
+			}
+		}
+		
+		return connectedWords;
+	}
+	
+	private void make_connections() {
+		// Make all the connections of the graph
+		
+		Enumeration<String> words = this.wordTable.keys();
+		while (words.hasMoreElements()) {
+			String word = words.nextElement();
+			this.wordTable.put(word, this.connected_words(word));
+		}
+	}
+	
+	private void create_table() {
+		// Creates the Hash table of the words from wordsArray
+		for (String word : this.wordArray) {
+			this.wordTable.put(word, new ArrayList<String>());
+		}
+	}
+	
+	public Boolean is_word(String word) {
+		// Returns if word is in the word list
+		return this.wordTable.containsKey(word);
+	}
+	
+	public void displayTable() {
+		Iterator<Entry<String, ArrayList<String>>> graph = 
+				this.wordTable.entrySet().iterator();
+		
+		while (graph.hasNext()) {
+			Map.Entry<String, ArrayList<String>> entry = graph.next();
+			
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		}
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		WordList list = new WordList();
+		
+		list.displayTable();
+	}
+}
